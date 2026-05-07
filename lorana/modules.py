b9abf2db7ce9ba00cefcc49ce1fcccafe8b164d5
khaledeldakhakhni <420251252@fa-hists.edu.eg>
2026-05-07 16:43:27 +0300

    eddit frontend home page

--- lorana/modules.py
+++ lorana/modules.py
@@ -34,7 +34,7 @@ class User(db.Model,UserMixin):
 
 class Product(db.Model):
     id          = db.Column(db.Integer,primary_key = True)
-    name        = db.Column(db.String(20) , unique = True , nullable = False)
-    price       = db.Column(db.String(20)  , nullable = False)
+    name        = db.Column(db.String(50) , unique = True , nullable = False)
+    price       = db.Column(db.String(50)  , nullable = False)
     description = db.Column(db.Text , nullable = False)
-    image       = db.Column(db.String(20), unique = False , nullable = False , default = 'default_product.jpg')
+    image       = db.Column(db.String(50), unique = False , nullable = False , default = 'default_product.jpg')
